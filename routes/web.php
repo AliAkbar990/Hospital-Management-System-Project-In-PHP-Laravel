4517251730c8228a98f8fdf015697629626e76c6
AliAkbar990 <akbarali9104515429@gmail.com>
2025-08-26 19:28:05 +0530

    I Can Update This Code

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\AppointmentController;
 // });
 
 Route::get('/', function () {
-    return view('user.home');
+    return view('home');
 });
 
 Route::get('login', function () {
